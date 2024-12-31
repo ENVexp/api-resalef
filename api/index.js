23f135a8ceb2c32d6da3b4d6a4dd60ee897a16f4
ceas <edu.alves1327@gmail.com>
2024-12-31 14:08:00 -0300

    teste host

--- api/index.js
+++ api/index.js
@@ -20,6 +20,25 @@ app.get('/teste', async (req, res)=>{
     }
 })
 
+app.get('/tes', (req, res)=>{
+    res.json({test: req.query.url, host: req.hostname})
+    
+    /*try {
+        const response = await axios.get(
+            "http://chead.cc:80/movie/Ronnyy/root@2424/990540.mp4",
+            {
+            responseType: 'stream'
+        });
+
+        res.setHeader('Content-Type', response.headers['content-type'] || 'application/octet-stream');
+        res.setHeader('Content-Length', response.headers['content-length'] || undefined);
+
+        response.data.pipe(res);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro', details: error.message });
+    }*/
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
