8c005d41e62ded0db1982d2fc06a2bfb268db713
ceas <edu.alves1327@gmail.com>
2024-12-31 10:54:58 -0300

    update link

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.get('/teste', async (req, res)=>{
     try {
         const response = await axios.get(
-            "http://tdck.cloud:443/series/Ronnyy/root@2424/679332.mp4",
+            "http://chead.cc:80/movie/Ronnyy/root@2424/990540.mp4",
             {
             responseType: 'stream'
         });
@@ -19,14 +19,6 @@ app.get('/teste', async (req, res)=>{
         res.status(500).json({ error: 'Erro', details: error.message });
     }
 })
-//update
-/*const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-*/
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
